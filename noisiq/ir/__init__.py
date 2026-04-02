f48a15913252c9db9bfffcdcd51d0ef37a2b6ab2
TeddyJ0nes <teddyjones304@gmail.com>
2026-04-02 12:41:46 -0700

    updated init.py in the ir file to be frontfacing to the public

--- noisiq/ir/__init__.py
+++ noisiq/ir/__init__.py
@@ -1,3 +1,18 @@
 from .circuit import Circuit, Operation
+from .gates import CNOT, H, I, S, T, X, Y, Z, Gate
 
-__all__ = ["Circuit", "Operation"]
+__all__ = [
+    # from circuit.py
+    "Circuit",
+    "Operation",
+    # from gates.py
+    "Gate",
+    "I",
+    "X",
+    "Y",
+    "Z",
+    "H",
+    "S",
+    "T",
+    "CNOT",
+]
